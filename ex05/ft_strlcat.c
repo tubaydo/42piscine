93ac82411ce929b10f6986e3735100031b8c29cb
tubaydo <111774892+tubaydo@users.noreply.github.com>
2022-09-03 19:36:41 +0300

    Add files via upload

---
+++ ex05/ft_strlcat.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taydogan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 05:07:51 by taydogan          #+#    #+#             */
+/*   Updated: 2022/09/03 08:09:00 by taydogan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>*/
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	while (src[i] != '\0')
+	{
+		if (j < size - 1)
+		{
+			dest[j] = src[i];
+		}
+		j++;
+		i++;
+	}
+	dest[j - 1] = '\0';
+	return (j);
+}
+/*
+int main (void)
+{
+	char src[] = "selamlar";
+    char dest [] = "hepinize ";
+    printf("%i \n", ft_strlcat(dest, src, 17));
+    printf("%s \n", dest);
+}
+*/
